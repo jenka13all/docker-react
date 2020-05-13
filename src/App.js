aadaf0c22bfa23f1b3429fe375e1040b5cab117b
jkoenig <jkoenig@notebooksbilliger.de>
2020-05-13 12:45:08 +0200

    Commit pre-existing project files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here is a link: change on the fly... ll
         </p>
         <a
           className="App-link"
